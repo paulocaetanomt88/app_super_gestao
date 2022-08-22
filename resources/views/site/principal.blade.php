a6d6af3228682d3b0f00a4275ee5e2c35276276c
Paulo Caetano <paulocaetanomt88@gmail.com>
2022-08-22 17:28:53 -0400

    modificação no componente da página principal

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -28,7 +28,7 @@
                 <h1>Contato</h1>
                 <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
                 @component('site.layouts._components.form_contato', ['classe' => 'borda-branca'])
-
+                    <p>A nossa equipe analisará sua mensagem e retornaremos em breve.</p>
                 @endcomponent
             </div>
         </div>
