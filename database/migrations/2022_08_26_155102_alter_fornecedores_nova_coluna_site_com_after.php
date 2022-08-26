d59e81e5a41c4eaf66120423e196ec0c5dddf50e
Paulo Caetano <paulocaetanomt88@gmail.com>
2022-08-26 16:47:11 -0400

    refactor: Adicionado campo site para tabela fornecedores

---
+++ database/migrations/2022_08_26_155102_alter_fornecedores_nova_coluna_site_com_after.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // adicionando a coluna site após a coluna nome em fornecedores
+        Schema::table('fornecedores', function(Blueprint $table) {
+            $table->string('site', 150)->after('nome')->nullable()->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // remove a coluna site de fornecedores
+        Schema::table('fornecedores', function(Blueprint $table) {
+            $table->dropColumn('site');
+        });
+    }
+};
