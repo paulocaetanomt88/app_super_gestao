1d45386a0836f0cade68190a3e078c6a16d82d72
Paulo Caetano <paulocaetanomt88@gmail.com>
2022-11-30 17:13:37 -0400

    personalização nas mensagens de erro dos campos

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,22 +2,35 @@
     @method('POST')
     @csrf
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
     <br>
     <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
     <br>
     <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    {{ $errors->has('email') ? $errors->first('email') : '' }}
     <br>
-    <select name="motivo_contato" class="{{ $classe }}">
-        <option value="0">Qual o motivo do contato?</option>
+    <select name="motivo_contatos_id" class="{{ $classe }}">
+        <option value="">Qual o motivo do contato?</option>
         
         @foreach ($motivo_contatos as $motivo_contato)
-            <option value="{{$motivo_contato->id}}" {{old('motivo_contato') == $motivo_contato->id ? 'selected' : ''}}>{{$motivo_contato->motivo_contato}}</option>
+            <option value="{{$motivo_contato->id}}" {{old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : ''}}>{{$motivo_contato->motivo_contato}}</option>
         @endforeach
     </select>
+    {{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : '' }}
     <br>
     <textarea name="mensagem"  class="{{ $classe }}">{{ (old('mensagem') != '') ? old('mensagem') : 'Digite aqui a sua mensagem' }}</textarea>
+    {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
 
-
+@if ($errors->any())
+    <div style="position: absolute; top: 0px; left: 0px; width: 100%; background: red; text-align: center;" >
+        <pre>
+            @foreach ($errors->all() as $erro)
+                {{ $erro }}
+            @endforeach
+        </pre>
+    </div>
+@endif
