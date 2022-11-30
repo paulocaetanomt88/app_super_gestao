5a68b166f46949fc9c4d1daf0eda99f50d7f9d4e
Paulo Caetano <paulocaetanomt88@gmail.com>
2022-11-30 17:12:19 -0400

    mudou 'motivo_contato' pra 'motivo_contatos_id'

--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -11,5 +11,5 @@ class SiteContato extends Model
 
     protected $table = "site_contatos";
 
-    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contatos_id', 'mensagem'];
 }
