c41f444e16d9730cd760d3a2f1864233648330aa
Paulo Caetano <paulocaetanomt88@gmail.com>
2023-01-13 15:48:45 -0400

    Relacionamento entre Pedidos e Produtos

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -14,4 +14,19 @@ class Pedido extends Model
     public function cliente() {
         return $this->belongsTo(Cliente::class, 'cliente_id', 'id');
     }
+
+    public function produtos() {
+        // Forma convencional
+        // return $this->belongsToMany(Produto::class, 'pedidos_produtos');
+
+        /* 
+            Forma usando model não-convencional
+            Parâmetros:
+            1 - Model do relacionamento N:N em relação a Model que estamos implementando
+            2 - Nome da Tabela auxiliar que armazena os registros de relacionamento
+            3 - Nome da Chave Extrangeira (FK) da tabela mapeada pelo model na tabela de relacionamento
+            4 - Nome da Chave Extrangeira (FK) da tabela mapeada pelo model utilizado no relacionamento que estamos implementando
+        */
+        return $this->belongsToMany(Item::class, 'pedidos_produtos', 'pedido_id', 'produto_id')->withPivot('created_at','quantidade');
+    }
 }
